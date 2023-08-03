2df49a9681947ef417b7737e38ce9290859638d5
asobiesk <adamsobieski1@gmail.com>
2023-08-03 15:50:11 +0200

    sitemap added

---
+++ src/app/sitemap.js
@@ -0,0 +1,28 @@
+export default async function Sitemap() {
+    return [
+        {
+            url: "https://heretostay.pl",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://heretostay.pl/about",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://heretostay.pl/offer",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://heretostay.pl/faq",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://heretostay.pl/blog",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://heretostay.pl/contact",
+            lastModified: new Date(),
+        },
+    ];
+}
